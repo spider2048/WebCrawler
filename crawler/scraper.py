3ed330a40888c3bac9c758bc97a9ff3abb5da493
Sp1d3R <aspy1337@gmail.com>
2024-01-28 23:10:48 +0530

    Restructure and really-async file save

--- crawler/scraper.py
+++ crawler/scraper.py
@@ -2,9 +2,10 @@ import asyncio
 import aiohttp
 import hashlib
 import logging
+import os
 import time
 import zlib
-from typing import Coroutine, List, Set
+from typing import Coroutine, Dict, List, Set, Type, Union, Any, Optional
 from urllib.parse import urldefrag, urljoin, urlparse
 
 import aiofiles
@@ -12,30 +13,43 @@ import networkx as nx
 import ujson as json  # Faster JSON
 from lxml import html
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
 class Scraper:
-    def __init__(self, profile_name: str, options, db, timestamp, debug=False) -> None:
+    def __init__(
+        self,
+        profile_name: str,
+        profile: Dict[str, Union[str, bool, int]],
+        crawl_options,
+    ) -> None:
         # Logging
         self.logger = logging.getLogger(__name__)
-        if debug:
+        if crawl_options["debug"]:
             self.logger.setLevel(logging.DEBUG)
         else:
             self.logger.setLevel(logging.INFO)
 
+        # Crawl options
+        self.options: Dict[str, Union[str, bool, int]] = crawl_options
+        self.graph_dir: str = os.path.join(
+            crawl_options["graph_dir"], crawl_options["timestamp"]
+        )
+
+        # Timestamps
+        self.current_time = int(time.time())
+
         # Parse/Get options
-        assert options.keys() == {"locations", "depth", "method", "same_domain"}
+        self.profile: str = profile_name
+        assert profile.keys() == {"locations", "depth", "method", "same_domain"}
         (
             (_, self.loc),
             (_, self.max_depth),
             (_, self.method),
             (_, self.same_domain),
-        ) = options.items()
-
-        self.profile: str = profile_name
+        ) = profile.items()
 
         # Graphing
         self.graph: nx.DiGraph = nx.DiGraph()
@@ -48,10 +62,13 @@ class Scraper:
         self.url_table: List[str] = []
 
         # Database (SQLAlchemy)
-        self.Base = declarative_base()
-        self.engine = create_async_engine(f"sqlite+aiosqlite:///{db}", echo=debug)
+        self.Base: Any = declarative_base()
+        self.engine: AsyncEngine = create_async_engine(
+            os.path.join("sqlite+aiosqlite:///", crawl_options["database_location"]),
+            echo=crawl_options["debug"],
+        )
 
-        self.Ty_UrlData = type(
+        self.Ty_UrlData: Type = type(
             "UrlData",
             (self.Base,),
             {
@@ -66,18 +83,17 @@ class Scraper:
         session_maker = sessionmaker(bind=self.engine, class_=AsyncSession)
         self.session = session_maker()
 
-        # Timestamps
-        self.timestamp = timestamp  # For graph's parent folder
-        self.current_time = int(time.time())
+        # File cache tasks
+        self.file_cache_tasks: List[Coroutine] = []
 
-    async def fetch_links(self, root_url: str, resp_text: str) -> Set:
-        links = set()
-        root_domain = urlparse(root_url).netloc
+    async def fetch_links(self, root_url: str, resp_text: str) -> Set[str]:
+        links: Set[str] = set()
+        root_domain: str = urlparse(root_url).netloc
         for a_tag in html.fromstring(resp_text).xpath("//a"):
-            href = a_tag.get("href")
+            href: Optional[str] = a_tag.get("href")
             if not href:
                 continue
-            link = urljoin(root_url, href)
+            link: str = urljoin(root_url, href)
 
             # Filter cross-site links
             if self.same_domain and urlparse(link).netloc != root_domain:
@@ -86,33 +102,47 @@ class Scraper:
             links.add(link)
         return links
 
-    async def store_graph(self):
+    async def store_graph(self) -> None:
         try:
-            with open(f"graphs/{self.timestamp}/{self.profile}.json", "w+") as fd:
+            self.logger.info("stored data in graph")
+            with open(os.path.join(self.graph_dir, f"{self.profile}.json"), "w+") as fd:
                 json.dump(nx.node_link_data(self.graph), fd)
         except Exception as err:
-            self.logger.error("failed to store_graph: %s", err)
+            self.logger.error("failed to store graph: %s", err)
+
+    async def cache_page(self, hash, data):
+        async with aiofiles.open(
+            os.path.join(self.options["cache_dir"], hash.hexdigest()), "wb+"
+        ) as fd:
+            await fd.write(data)
 
     async def crawl_worker(self, url: str, websession: aiohttp.ClientSession):
         self.visited_urls.add(url)
         self.logger.debug("visiting: %s", url)
+
         try:
+            # Fetch page and links
             content: str = await self.fetch_page(websession, url)
             links: Set[str] = await self.fetch_links(url, content)
 
+            # Compress page and calculate hash
             cdata: bytes = zlib.compress(content.encode())
             hash: hashlib._Hash = hashlib.sha1(cdata)
 
+            # Schedule to save files
+            self.file_cache_tasks.append(self.cache_page(hash, cdata))
+
+            # Add page data to database
             d = self.Ty_UrlData()
             d.url = url
             d.time = self.current_time
             d.hash = hash.hexdigest()
             self.session.add(d)
 
-            async with aiofiles.open(f"data/{hash.hexdigest()}", "wb+") as fd:
-                await fd.write(cdata)
-
+            # Add links to graph
             self.graph.add_edges_from((url, l) for l in links)
+
+            # Update next queue
             self.new_queue.update(links)
         except Exception as err:
             self.logger.error(err)
@@ -135,13 +165,25 @@ class Scraper:
             self.queue = self.new_queue - self.visited_urls
             self.new_queue.clear()
 
-        print(f"[{self.profile}] Crawled: {len(self.visited_urls)} URLs")
-        print(f"[{self.profile}] queue size: {len(self.queue)}")
+        self.logger.info("[%s] Crawled: %d URLs", self.profile, len(self.visited_urls))
+        self.logger.info("[%s] queue size: %d", self.profile, len(self.queue))
 
+        # Store graph
         await self.store_graph()
+
+        # Commit SQL data
         await self.session.commit()
+
+        # Cleanup SQL session
         await self.session.close()
+
+        # Cleanup Web session
         await websession.close()
+
+        # Save all pending files
+        await asyncio.gather(*self.file_cache_tasks)
+
+        self.logger.info("Saved %d files to disk", len(self.file_cache_tasks))
 
     async def fetch_page(self, session: aiohttp.ClientSession, root_url: str):
         self.logger.debug("fetch page: %s", root_url)
